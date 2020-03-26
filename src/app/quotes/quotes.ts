6620e854b52fa483e0f21aefd3015f0a99abddf5
Dhosio <danielgithiomi@gmail.com>
2020-03-26 17:00:31 +0300

    Input property binding for the author details resolved

--- src/app/quotes/quotes.ts
+++ src/app/quotes/quotes.ts
@@ -2,6 +2,6 @@ export class Quotes {
    additionalInfo:boolean;
    constructor(public id:number, public author:string, public actQuote:string, public upVote:number,
        public downVote:number){
-          this.additionalInfo=false;
+         this.additionalInfo=false;
        }
 }
